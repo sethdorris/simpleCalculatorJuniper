1ab5b4f0a83f179dcf4174cd09ed20fd74b77264
sethdorris <sethc.dorris@gmail.com>
2015-11-18 20:58:25 -0600

    Created Parse Class and Tests. Can Extract number 1 and number 2 from the users input, still need to create the method to extract the operator

---
+++ SimpleCalculator/Parse.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace SimpleCalculator
+{
+    public class Parse
+    {
+        public string getOperator(string input)
+        {
+            if (AreSpaces(input))
+            {
+                string inputWithOutSpaces = removeSpaces(input);
+            }
+            return "+";
+        }
+
+        public int getNumberOne(string input)
+        {
+            int num1;
+            string[] inputArray = Regex.Split(input, @"([-+*/])");
+            int.TryParse(inputArray[0], out num1);
+            return num1;
+        }
+
+        public int getNumberTwo(string input)
+        {
+            int num2;
+            string[] inputArray = Regex.Split(input, @"([-+*/])");
+            int.TryParse(inputArray[2], out num2);
+            return num2;
+        }
+
+        public string[] getInputStringArray(string input)
+        {
+            string[] inputArray = Regex.Split(input, @"([-+*/])");
+            return inputArray;
+        }
+
+        public string removeSpaces(string input)
+        {
+            string[] noSpaces = input.Split(' ');
+            input = String.Join("", noSpaces);
+            return input;
+        }
+
+        public bool AreSpaces(string input)
+        {
+            int indexOfSpaces = input.IndexOf(" ");
+            if (indexOfSpaces > 0)
+            {
+                return true;
+            } else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ SimpleCalculatorTests/ParseTest.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SimpleCalculator;
+
+namespace SimpleCalculatorTests
+{
+    [TestClass]
+    public class ParseTest
+    {
+        [TestMethod]
+        public void ParseEnsureICanCreateAnInstance()
+        {
+            Parse p = new Parse();
+            Assert.IsNotNull(p);
+        }
+
+        [TestMethod]
+        public void ParseEnsureICanGetAdditionOperator()
+        {
+            Parse p = new Parse();
+            string input = "2 + 2";
+            Assert.AreEqual("+", p.getOperator(input));
+        }
+
+        [TestMethod]
+        public void ParseDetectingIfThereIsSpaces()
+        {
+            Parse p = new Parse();
+            string input = "2 + 2";
+            Assert.IsTrue(p.AreSpaces(input));
+        }
+
+        [TestMethod]
+        public void ParseThereAreNoSpaces()
+        {
+            Parse p = new Parse();
+            string input = "2+2";
+            Assert.IsFalse(p.AreSpaces(input));
+        }
+
+        [TestMethod]
+        public void ParseEnsureICanGetFirstNumber()
+        {
+            Parse p = new Parse();
+            string input = "2+3";
+            Assert.AreEqual(2, p.getNumberOne(input));
+        }
+
+        [TestMethod]
+        public void ParseTestSplitInputStringOnOperatorsWithoutSpaces()
+        {
+            Parse p = new Parse();
+            string input = "5+4";
+            string[] expected = { "5", "+", "4" };
+            CollectionAssert.AreEqual(expected, p.getInputStringArray(input));
+        }
+
+        [TestMethod]
+        public void ParseEnsureICanGetRidOfSpaces()
+        {
+            Parse p = new Parse();
+            string input = "2 + 2";
+            Assert.AreEqual("2+2", p.removeSpaces(input));
+        }
+
+        [TestMethod]
+        public void ParseEnsureICanGetSecondNumber()
+        {
+            Parse p = new Parse();
+            string input = "2+1";
+            Assert.AreEqual(1, p.getNumberTwo(input));
+        }
+    }
+}
