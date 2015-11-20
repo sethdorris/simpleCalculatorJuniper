84ac01bf04d735d1935869fa169f326873b9e472
sethdorris <sethc.dorris@gmail.com>
2015-11-19 18:45:57 -0600

    Created Evaluate Class that performs the mathematical operations, and created the Evaluate Class Tests

---
+++ SimpleCalculator/Evaluate.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace SimpleCalculator
+{
+    public class Evaluate
+    {
+        public int ExecuteMethod(int num1, int num2, string oper)
+        {
+            switch (oper)
+            {
+                case "+":
+                   return Addition(num1, num2);
+                case "-":
+                    return Subtraction(num1, num2);
+                case "*":
+                    return Multiply(num1, num2);
+                case "/":
+                    return Divide(num1, num2);
+                case "%":
+                    return Modulo(num1, num2);
+                default:
+                    break;
+            }
+            throw new InvalidOperationException();
+        }
+        public int Addition(int v1, int v2)
+        {
+            return v1 + v2;
+        }
+
+        public int Subtraction(int v1, int v2)
+        {
+            return v1 - v2;
+        }
+
+        public int Multiply(int v1, int v2)
+        {
+            return v1 * v2;
+        }
+
+        public int Divide(int v1, int v2)
+        {
+            return v1 / v2;
+        }
+
+        public int Modulo(int v1, int v2)
+        {
+            return v1 % v2; 
+        }
+    }
+}
---
+++ SimpleCalculatorTests/EvaluateTest.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SimpleCalculator;
+
+namespace SimpleCalculatorTests
+{
+    [TestClass]
+    public class EvaluateTest
+    {
+        [TestMethod]
+        public void EnsureICanCreateAnInstanceOfEvaluateClass()
+        {
+            Evaluate e = new Evaluate();
+            Assert.IsNotNull(e);
+        }
+
+        [TestMethod]
+        public void EvaluateAdditionExpression()
+        {
+            Evaluate e = new Evaluate();
+            int expected = 4;
+            Assert.AreEqual(expected, e.Addition(2, 2));
+        }
+
+        [TestMethod]
+        public void EvaluateSubtractionExpression()
+        {
+            Evaluate e = new Evaluate();
+            int expected = 4;
+            Assert.AreEqual(expected, e.Subtraction(6, 2));
+        }
+
+        [TestMethod]
+        public void EvaluateMultiplicationExpression()
+        {
+            Evaluate e = new Evaluate();
+            int expected = 10;
+            Assert.AreEqual(expected, e.Multiply(2, 5));
+        }
+
+        [TestMethod]
+        public void EvaluateDivisionExpression()
+        {
+            Evaluate e = new Evaluate();
+            int expected = 2;
+            Assert.AreEqual(expected, e.Divide(4, 2));
+        }
+
+        [TestMethod]
+        public void EvaluateModuloExpression()
+        {
+            Evaluate e = new Evaluate();
+            int expected = 0;
+            Assert.AreEqual(expected, e.Modulo(4, 2));
+        }
+
+        [TestMethod]
+        public void EvaluateEnsureICanEvaluateInputCorrectlyAddition()
+        {
+            Parse p = new Parse();
+            Evaluate e = new Evaluate();
+            string input = "4 + 3";
+            int num1 = p.getNumberOne(input);
+            int num2 = p.getNumberTwo(input);
+            string oper = p.getOperator(input);
+            int expected = 7;
+            Assert.AreEqual(expected, e.ExecuteMethod(num1, num2, oper));
+        }
+    }
+}
