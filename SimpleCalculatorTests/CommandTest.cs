74ddbd78bd7b3841aac66a26d09535d128680f7f
sethdorris <sethc.dorris@gmail.com>
2015-12-06 19:31:34 -0600

    Created myStack class and basic implementation of a lastq command

---
+++ SimpleCalculatorTests/CommandTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections;
+using SimpleCalculator;
+
+namespace SimpleCalculatorTests
+{
+    [TestClass]
+    public class CommandTest
+    {
+        [TestMethod]
+        public void CommandEnsureICanCreateAnInstance()
+        {
+            myStack q = new myStack();
+            Assert.IsNotNull(q);
+        }
+
+        [TestMethod]
+        public void CommandEnsureICanGetReturnFromLastQCommand()
+        {
+            Parse p = new Parse();
+            Evaluate e = new Evaluate();
+            string input = "2 + 2";
+            int num1 = p.getNumberOne(input);
+            int num2 = p.getNumberTwo(input);
+            string oper = p.getOperator(input);
+            int answer = e.ExecuteMethod(num1, num2, oper);
+            string expected = "2 + 2";
+            string actual = e.lastExpression();
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
--- SimpleCalculatorTests/EvaluateTest.cs
+++ SimpleCalculatorTests/EvaluateTest.cs
@@ -7,6 +7,69 @@ namespace SimpleCalculatorTests
     [TestClass]
     public class EvaluateTest
     {
+        [TestMethod]
+        public void EvaluateTestEnsureICanAdd()
+        {
+            Evaluate e = new Evaluate();
+            Parse p = new Parse();
+            string input = "2 + 2";
+            int num1 = p.getNumberOne(input);
+            int num2 = p.getNumberTwo(input);
+            string oper = p.getOperator(input);
+            int expected = 4;
+            Assert.AreEqual(expected, e.ExecuteMethod(num1, num2, oper));
+        }
 
+        [TestMethod]
+        public void EvaluateTestEnsureICanSubtract()
+        {
+            Evaluate e = new Evaluate();
+            Parse p = new Parse();
+            string input = "2 - 2";
+            int num1 = p.getNumberOne(input);
+            int num2 = p.getNumberTwo(input);
+            string oper = p.getOperator(input);
+            int expected = 0;
+            Assert.AreEqual(expected, e.ExecuteMethod(num1, num2, oper));
+        }
+
+        [TestMethod]
+        public void EvaluateTestEnsureICanMultiply()
+        {
+            Evaluate e = new Evaluate();
+            Parse p = new Parse();
+            string input = "2 * 2";
+            int num1 = p.getNumberOne(input);
+            int num2 = p.getNumberTwo(input);
+            string oper = p.getOperator(input);
+            int expected = 4;
+            Assert.AreEqual(expected, e.ExecuteMethod(num1, num2, oper));
+        }
+
+        [TestMethod]
+        public void EvaluateTestEnsureICanDivide()
+        {
+            Evaluate e = new Evaluate();
+            Parse p = new Parse();
+            string input = "4 / 2";
+            int num1 = p.getNumberOne(input);
+            int num2 = p.getNumberTwo(input);
+            string oper = p.getOperator(input);
+            int expected = 2;
+            Assert.AreEqual(expected, e.ExecuteMethod(num1, num2, oper));
+        }
+
+        [TestMethod]
+        public void EvaluateTestEnsureICanModulo()
+        {
+            Evaluate e = new Evaluate();
+            Parse p = new Parse();
+            string input = "2 % 2";
+            int num1 = p.getNumberOne(input);
+            int num2 = p.getNumberTwo(input);
+            string oper = p.getOperator(input);
+            int expected = 0;
+            Assert.AreEqual(expected, e.ExecuteMethod(num1, num2, oper));
+        }
     }
 }
