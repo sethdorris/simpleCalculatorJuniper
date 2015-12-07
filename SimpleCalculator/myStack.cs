294d678cc1ed00d8cfc5bac1786f2b37309f7ece
sethdorris <sethc.dorris@gmail.com>
2015-12-06 19:30:41 -0600

    Created myStack class that will hold the previously evaluated mathematical expression.

---
+++ SimpleCalculator/myStack.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCalculator
+{
+   public class myStack
+    {
+        public string lastq { get; set; }
+        public string last { get; set; }
+        public myStack()
+        {
+
+        }
+        public myStack(int num1, int num2, string oper)
+        {
+            lastq = num1.ToString() + " " + oper + " " + num2.ToString();
+        }
+    }
+}
