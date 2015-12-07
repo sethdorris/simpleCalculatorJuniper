0de81461df07310724c219a8bb80988ea8dacb6f
sethdorris <sethc.dorris@gmail.com>
2015-12-06 19:30:09 -0600

    Evaluate class now has stack class set as a public property.

--- SimpleCalculator/Evaluate.cs
+++ SimpleCalculator/Evaluate.cs
@@ -3,30 +3,64 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SimpleCalculator;
 
 
 namespace SimpleCalculator
 {
     public class Evaluate
     {
-        public int ExecuteMethod(string num1, string num2, string oper)
+        private myStack s = new myStack();
+        public int ExecuteMethod(int num1, int num2, string oper)
         {
+            s.lastq = num1.ToString() + " " + oper + " " + num2.ToString();
             switch (oper)
             {
                 case "+":
-             //      return Addition(num1, num2);
+                   return Addition(num1, num2);
                 case "-":
-               //     return Subtraction(num1, num2);
+                   return Subtraction(num1, num2);
                 case "*":
-                 //   return Multiply(num1, num2);
+                   return Multiply(num1, num2);
                 case "/":
-                   // return Divide(num1, num2);
+                   return Divide(num1, num2);
                 case "%":
-                   // return Modulo(num1, num2);
+                   return Modulo(num1, num2);
                 default:
                     break;
             }
             throw new InvalidOperationException();
+        }
+
+        private int Modulo(int num1, int num2)
+        {
+            return num1 % num2;
+        }
+
+        private int Divide(int num1, int num2)
+        {
+            return num1 / num2;
+        }
+
+        private int Multiply(int num1, int num2)
+        {
+
+            return num1 * num2;
+        }
+
+        private int Subtraction(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+
+        public int Addition(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public string lastExpression()
+        {
+            return s.lastq;
         }
     }
 }
