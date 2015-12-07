a0d1fc9a12d0562aebf1caddb95fd1cf46bee37f
sethdorris <sethc.dorris@gmail.com>
2015-12-06 19:31:06 -0600

    Created the very basic while loop to run the console application

--- SimpleCalculator/Program.cs
+++ SimpleCalculator/Program.cs
@@ -10,6 +10,30 @@ namespace SimpleCalculator
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("What is your name?");
+            string userName = Console.ReadLine();
+            Parse p = new Parse();
+            Evaluate e = new Evaluate();
+            bool run = true;
+
+            while (run)
+            {
+                Console.WriteLine(userName + " Define your mathematical operation");
+                string input = Console.ReadLine();
+                if (input.ToUpper() == "LASTQ")
+                {
+                    Console.WriteLine(e.lastExpression());
+                    run = false;
+                    break;
+                }
+                string oper = p.getOperator(input);
+                int num1 = p.getNumberOne(input);
+                int num2 = p.getNumberTwo(input);
+                int answer = e.ExecuteMethod(num1, num2, oper);
+                Console.WriteLine("The answer to your operation is: " + answer);
+            }
+            Console.WriteLine("Press any key to exit");
+            Console.ReadLine();
         }
     }
 }
